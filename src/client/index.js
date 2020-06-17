0a1e7fbe049c49dee45c91a2057fd26c929a7f39
Andy Woerner <andy.woerner@icloud.com>
2020-06-17 12:01:39 +0200

    style: add some basic style

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,14 @@
 // import scripts
 
 // import styles
-import "./styles/style.scss"
+import "./styles/resets.scss"
+import "./styles/base.scss"
+import "./styles/footer.scss"
+import "./styles/header.scss"
+import "./styles/form.scss"
+
+// import images
+import background from "./media/background.jpg"
 
 
 // exports
